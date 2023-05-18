5569d832adf69b44469976d59c08a28985f0483b
Al-Ali Ibrahim <ibrahim@Al-Alis-MacBook-Air.local>
2023-05-18 21:56:13 +0100

    progress..

--- main.go
+++ main.go
@@ -1,72 +1,44 @@
 package main
 
 import (
-	"ascii_art/myFunctions"
-	"flag"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 )
 
 func main() {
-	flag.Usage = func() {
-		fmt.Println("Usage: ascii-art [OPTIONS] WORD")
-		flag.PrintDefaults()
-	}
-
-	// Define the color flag
-	flag.String("colour", "", "Specify the color for the letters")
-
-	// Parse the command-line arguments
-	flag.Parse()
-
-	// Retrieve the positional argument (word)
-	args := flag.Args()
-	if len(args) < 1 {
-		flag.Usage()
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify a word.")
 		return
 	}
 
-	word := args[0]
+	// Retrieve the first command-line argument
+	word := os.Args[1]
+	if word == "" {
+		fmt.Println()
+		return
+	}
 
 	// Set the default filename to "standard.txt"
 	filename := "fonts/standard.txt"
 
-	switch len(args) {
-	case 1:
-		// Only word argument provided, perform ASCII art on the word using default font
-		// No need to do anything here
-	case 2:
-		// Second argument is provided, check if it's a color flag
-		if args[1] == "--color" {
-			if len(args) < 4 {
-				fmt.Println("Please specify both a color and a word.")
-				return
-			}
-			color := args[2]
-			word = myFunctions.ApplyColour(&word, color)
-		
-			if len(args) < 5 {
-				fmt.Println("Please specify a font.")
-				return
-			}
-			font := args[3]
-			switch font {
-			case "shadow":
-				filename = "fonts/shadow.txt"
-			case "standard":
-				filename = "fonts/standard.txt"
-			case "thinkertoy":
-				filename = "fonts/thinkertoy.txt"
-			default:
-				fmt.Println("Please specify a valid font. Valid fonts are shadow, standard, and thinkertoy.")
-				return
-			}
+	// Check if there are three command-line arguments
+	if len(os.Args) == 3 {
+		// Retrieve the second command-line argument
+		font := os.Args[2]
+
+		// Load the contents of the appropriate file based on the command-line argument
+		switch font {
+		case "shadow":
+			filename = "fonts/shadow.txt"
+		case "standard":
+			filename = "fonts/standard.txt"
+		case "thinkertoy":
+			filename = "fonts/thinkertoy.txt"
+		default:
+			fmt.Println("Please specify a valid font. Valid fonts are shadow, standard, and thinkertoy.")
+			return
 		}
-	default:
-		fmt.Println("Invalid number of arguments.")
-		return
 	}
 
 	content, err := os.ReadFile(filename)
@@ -74,7 +46,6 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
 	var lines []string
 	if filename == "fonts/thinkertoy.txt" {
 		lines = strings.Split(string(content), "\r\n")
@@ -82,29 +53,15 @@ func main() {
 		lines = strings.Split(string(content), "\n")
 	}
 
-	// Generate the ASCII art for the specified word
-	var asciiArt strings.Builder
+	// Print the specified word with the selected font
 	for h := 1; h < 9; h++ {
 		for _, l := range word {
 			for lineIndex, line := range lines {
 				if lineIndex == (int(l)-32)*9+h {
-					asciiArt.WriteString(line)
+					fmt.Print(line)
 				}
 			}
 		}
-		asciiArt.WriteString("\n")
-	}
-
-	// Output the ASCII art
-	output := asciiArt.String()
-
-	// Check if the output needs to be piped into another command
-	if myFunctions.IsPiped() {
-		cmd := exec.Command("cat", "-e")
-		cmd.Stdin = strings.NewReader(output)
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		fmt.Print(output)
+		fmt.Println()
 	}
 }
--- myFunctions/colour.go
+++ myFunctions/colour.go
@@ -1,41 +1,37 @@
 package myFunctions
 
-import (
-	"strings"
-)
+import "strings"
 
-// ApplyColor applies the specified color to the input string
-// applyColour applies the specified colour to the input string
-func ApplyColour(word *string, color string) string {
-	// Convert the color to uppercase for case-insensitive matching
-	colour := strings.ToUpper(color)
+func ApplyColour(text string, colour string) string {
+	// Convert the colour to uppercase for case-insensitive matching
+	colour = strings.ToUpper(colour)
 
-	// Apply the color based on the RGB values
+	// Apply the colour based on the RGB values
 	switch colour {
 	case "RED":
-		return "\033[31m" + *word + "\033[0m" // Red color
+		return "\033[31m" + text + "\033[0m" // Red color
 	case "GREEN":
-		return "\033[32m" + *word + "\033[0m" // Green color
+		return "\033[32m" + text + "\033[0m" // Green color
 	case "BLUE":
-		return "\033[34m" + *word + "\033[0m" // Blue color
+		return "\033[34m" + text + "\033[0m" // Blue color
 	case "CYAN":
-		return "\033[36m" + *word + "\033[0m" // Cyan color
+		return "\033[36m" + text + "\033[0m" // Cyan color
 	case "MAGENTA":
-		return "\033[35m" + *word + "\033[0m" // Magenta color
+		return "\033[35m" + text + "\033[0m" // Magenta color
 	case "YELLOW":
-		return "\033[33m" + *word + "\033[0m" // Yellow color
+		return "\033[33m" + text + "\033[0m" // Yellow color
 	case "WHITE":
-		return "\033[37m" + *word + "\033[0m" // White color
+		return "\033[37m" + text + "\033[0m" // White color
 	case "ORANGE":
-		return "\033[38;5;208m" + *word + "\033[0m" // Orange color
+		return "\033[38;5;208m" + text + "\033[0m" // Orange color
 	case "PINK":
-		return "\033[38;5;205m" + *word + "\033[0m" // Pink color
+		return "\033[38;5;205m" + text + "\033[0m" // Pink color
 	case "PURPLE":
-		return "\033[38;5;165m" + *word + "\033[0m" // Purple color
+		return "\033[38;5;165m" + text + "\033[0m" // Purple color
 	case "BLACK":
-		return "\033[38;5;232m" + *word + "\033[0m" // Black color
+		return "\033[38;5;232m" + text + "\033[0m" // Black color
 	default:
-		// If the provided color is not supported, return the original word
-		return *word
+		// If the provided colour is not supported, return the original text
+		return text
 	}
 }
